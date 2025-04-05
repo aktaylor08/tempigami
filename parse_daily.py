cd998476b4172736d4cf1a7451c8c94313b63c08
Adam Taylor <aktaylor08@gmail.com>
2025-04-04 21:39:30 -0500

    Python part

---
+++ parse_daily.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import requests
+import datetime
+import numpy as np
+import pandas as pd
+
+from calendar import monthrange
+
+
+class Element:
+    def __init__(self, code: str, name: str, fun=None) -> None:
+        self.code = code
+        self.name = name
+        if fun:
+            self.fun = np.vectorize(fun)
+        else:
+            self.fun = None
+
+    def process_row(self, row):
+        if self.fun:
+            return self.fun(row)
+        else:
+            return row
+
+
+ELEMENTS = [
+    Element("TMAX", "Temp Max", fun=lambda x: np.round((x / 10.0) * (9.0 / 5.0) + 32)),
+    Element("TMIN", "Temp Max", fun=lambda x: np.round((x / 10.0) * (9.0 / 5.0) + 32)),
+    Element("PRCP", "Precipitation", fun=lambda x: x * 0.039),
+    Element("SNWD", "Snow Depth", fun=lambda x: x * 0.039),
+    Element("SNOW", "Snow", fun=lambda x: x * 0.039),
+]
+
+MAP = {x.code: (idx, x) for idx, x in enumerate(ELEMENTS)}
+
+
+def parse_to_df(data):
+    parsed_data = np.empty((0, len(ELEMENTS)))
+    dates = []
+    starts = {}
+    for line in data:
+        if len(line) < 1:
+            continue
+        year = int(line[11:15])
+        month = int(line[15:17])
+        size = monthrange(year, month)[1]
+        if (year, month) in starts:
+            row = starts[(year, month)]
+        else:
+            row = parsed_data.shape[0]
+            new_month = np.empty((size, len(ELEMENTS)))
+            new_month.fill(np.nan)
+            parsed_data = np.append(parsed_data, new_month, axis=0)
+            for x in range(size):
+                dates.append(datetime.date(year, month, x + 1))
+            starts[(year, month)] = row
+        element = line[17:21]
+        if element in MAP:
+            col, thing = MAP[element]
+            start_idx = 21
+            data = []
+            for _ in range(size):
+                value = int(line[start_idx : start_idx + 5])
+                mflag = line[start_idx + 5 : start_idx + 6]
+                qflag = line[start_idx + 6 : start_idx + 7]
+                sflag = line[start_idx + 7 : start_idx + 8]
+                if mflag != " ":
+                    print("MFLAG", value, mflag)
+                if qflag != " " or value == -9999:
+                    value = np.nan
+                data.append(value)
+                start_idx += 8
+            parsed_data[row : row + size, col] = thing.process_row(np.array(data))
+    return pd.DataFrame(
+        data=parsed_data, index=dates, columns=[x.code for x in ELEMENTS]
+    )
+
+
+def get_stations_data(station):
+    url = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/all/" + station + ".dly"
+    response = requests.get(url)
+    return response.text.split("\n")
---
+++ server.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from parse_daily import get_stations_data, parse_to_df
+import numpy as np
+
+
+origins = [
+    "http://localhost:3000",  # React development server
+    "http://yourfrontend.com",  # Your deployed frontend URL
+]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow only specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+app.mount("/static", StaticFiles(directory="frontend/build/static"), name="static")
+
+
+@app.get("/tempgami/{station}")
+def get_data(station: str):
+    data = get_stations_data(station)
+    df = parse_to_df(data)
+    min_t = int(df["TMIN"].min())
+    max_t = int(df["TMAX"].max())
+    grid = np.zeros(((max_t - min_t + 1, max_t - min_t + 1)))
+    unique = 0
+    last_10 = []
+    firstlast = [
+        [["", ""] for _ in range(max_t - min_t + 1)] for _ in range(max_t - min_t + 1)
+    ]
+    axis = np.linspace(min_t, max_t, endpoint=True, num=max_t - min_t + 1)
+    for row in df.iterrows():
+        if np.isnan(row[1]["TMIN"]) or np.isnan(row[1]["TMAX"]):
+            continue
+        y_idx = int(row[1]["TMIN"] - min_t)
+        x_idx = int(row[1]["TMAX"] - min_t)
+        if grid[x_idx, y_idx] == 0:
+            unique += 1
+            firstlast[x_idx][y_idx][0] = row[0]
+            firstlast[x_idx][y_idx][1] = row[0]
+            last_10.append([row[0], row[1]["TMIN"], row[1]["TMAX"]])
+            last_10 = last_10[-10:]
+        else:
+            firstlast[x_idx][y_idx][1] = row[0]
+
+        grid[x_idx, y_idx] += 1
+    return {
+        "station": station,
+        "grid": grid.tolist(),
+        "temps": axis.tolist(),
+        "firstlast": firstlast,
+        "maxTemp": min_t,
+        "minTemp": max_t,
+        "unique": unique,
+        "last10": last_10,
+    }
+
+    # Defines a route handler for `/*` essentially.
+
+
+@app.get("/{full_path:path}")
+async def serve_react_app(full_path: str):
+    index_path = "frontend/build/index.html"
+    return FileResponse(index_path)
